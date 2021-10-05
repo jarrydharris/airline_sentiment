16bc4d89f20c7526e9b2b37571e1c36882b9b0e5
jarryd <jhar0383@uni.sydney.edu.au>
2021-10-05 20:02:28 +1100

    moving around files, final setup for app, added some docstrings

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from dash import html
 from dash.dependencies import Input, Output, State
 from google.api_core.client_options import ClientOptions
 from google.cloud import automl_v1
-import json
+from waitress import serve
 
 project_id = "sentiment-analysis-01"
 model_id = "TST1490117531589935104"
@@ -39,12 +39,13 @@ app.layout = html.Div(children=[
 ])
 
 def get_prediction(payload, model_name):
+    """
+    Sends a payload to the autoML api to use pretrained model. Returns request
+    """
     options = ClientOptions(api_endpoint='automl.googleapis.com')
     prediction_client = (
         automl_v1.PredictionServiceClient(client_options=options))
-
     request = prediction_client.predict(name=model_name, payload=payload)
-
     return request  # waits until request is returned
 
 @app.callback(
@@ -53,6 +54,11 @@ def get_prediction(payload, model_name):
     [State('my-input', 'value')]
 )
 def update_output_div(n_clicks, input_value):
+    """
+    Main workhorse function, updates the contents of the output div when
+    n_clicks changes (button is pressed). Calls get_prediction and returns
+    a result based on the output of the model
+    """
     payload = {'text_snippet': {'content': input_value, 
         'mime_type': 'text/plain'} }
     prediction = get_prediction(payload, model_name)
@@ -67,4 +73,5 @@ def update_output_div(n_clicks, input_value):
         return 'Output: {}'.format("Error")
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    serve(app.server, host="0.0.0.0", port=8080)
+    
